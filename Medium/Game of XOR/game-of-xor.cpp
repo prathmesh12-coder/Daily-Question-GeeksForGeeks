77672687a48b7a12e667df3c99469f315683acb3
Prathmesh Sapate <85802377+prathmesh12-coder@users.noreply.github.com>
2023-12-18 09:22:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Game of XOR/game-of-xor.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int gameOfXor(int n , int a[]) {
+        int x=0;
+        for(int i=0;i<n;i++){
+            bool odd=((n-i)*(i+1))&1;
+            x^=odd?a[i]:0;
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+        int A[N];
+        for(int i=0 ; i<N ; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.gameOfXor(N,A) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
