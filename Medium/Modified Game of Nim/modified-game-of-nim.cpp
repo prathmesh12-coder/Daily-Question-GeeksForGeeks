3f8923878958b6cfcabc06bef1ed065e2d5b7b79
Prathmesh Sapate <85802377+prathmesh12-coder@users.noreply.github.com>
2023-12-20 09:33:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Modified Game of Nim/modified-game-of-nim.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        if(n%2==0)return 1;
+        int xorsum=0;
+        for(int i=0;i<n;i++){
+            xorsum=xorsum^A[i];
+        }
+        return xorsum==0?1:2;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int A[n];
+        for(int i = 0;i < n;i++)
+            cin>>A[i];
+        
+        Solution ob;
+        cout<<ob.findWinner(n, A)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
