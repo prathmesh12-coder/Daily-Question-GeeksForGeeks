7dc7c235d64c8193acddbb7535c16b0cb38abc98
Prathmesh Sapate <85802377+prathmesh12-coder@users.noreply.github.com>
2024-01-11 10:30:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Remove K Digits/remove-k-digits.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+
+class Solution {
+  public:
+    string removeKdigits(string S, int k) {
+        stack<char>st;
+        for(int i=0; i<S.size(); i++){
+            while(!st.empty() and st.top()>S[i] and k){
+                st.pop();
+                k--;
+            }
+            st.push(S[i]);
+        }
+        if(st.empty())
+            return "0";
+        while(k--)
+            st.pop();
+        string res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        reverse(res.begin(),res.end());
+        int i=0;
+        while(res[i]=='0')
+            i++;
+        if(i==res.size())
+            return "0";
+        return res.substr(i);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        int K;
+        cin >> K;
+        Solution obj;
+        cout << obj.removeKdigits(S, K) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
